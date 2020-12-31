2f50230b62ca242553856efd89229fa13cbb0da8
gaojianhua <jianhua.gao@foxmail.com>
2020-12-31 10:26:30 +0800

    edit

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,6 +1,6 @@
 import React, { memo } from "react";
 import PageWrap  from "./style";
-import HomeShow from "../home/c-comp/home-show";
+import HomeShow from "@p/Home/c-comp/home-show";
 import MainCard from "./c-comp/main-card";
 
 export default memo(function Blog() {
